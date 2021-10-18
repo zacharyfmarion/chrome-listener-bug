4a43d694c46f33bc6f7a21292bda63cc1160992e
Zachary Marion <zac.marion@coinbase.com>
2021-10-17 22:35:21 -0400

    Format

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class FriendDatabase extends Dexie {
   constructor() {
     super("FriendDatabase");
     this.version(1).stores({
-      friends: "++id,name,age"
+      friends: "++id,name,age",
     });
     this.friends = this.table("friends");
   }
@@ -11,13 +11,13 @@ class FriendDatabase extends Dexie {
 const db = new FriendDatabase();
 
 setInterval(() => {
-	db.transaction("r", db.friends, () => {
-		db.friends
-		.where("age")
-		.below(25)
-		.toArray()
-		.then((data) => {
-		console.log(`My young friends: ${JSON.stringify(data)}`);
-		});
-	});
+  db.transaction("r", db.friends, () => {
+    db.friends
+      .where("age")
+      .below(25)
+      .toArray()
+      .then((data) => {
+        console.log(`My young friends: ${JSON.stringify(data)}`);
+      });
+  });
 }, 5000);
